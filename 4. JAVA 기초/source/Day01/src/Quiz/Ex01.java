556c6889a5af6411a0dd5c0d9d6c3b9c6a6449f6
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-09 17:30:18 +0900

    quiz

---
+++ 4. JAVA 기초/source/Day01/src/Quiz/Ex01.java
@@ -0,0 +1,10 @@
+package Quiz;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b =3.0;
+
+        System.out.println((int)(a/b));
+    }
+}
---
+++ 4. JAVA 기초/source/Day01/src/Quiz/Ex03.java
@@ -0,0 +1,28 @@
+package Quiz;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        int floor = 3;
+
+        switch(floor) {
+            case 1 :
+                System.out.println(floor+"층 약국입니다.");
+                break;
+            case 2 :
+                System.out.println(floor+"층 정형외과입니다.");
+                break;
+            case 3 :
+                System.out.println(floor+"층 피부과입니다.");
+                break;
+            case 4 :
+                System.out.println(floor+"층 치과입니다.");
+                break;
+            case 5 :
+                System.out.println(floor+"층 헬스클럽입니다.");
+                break;
+            default:
+                System.out.println(floor+"층은 없습니다.");
+        }
+    }
+}
