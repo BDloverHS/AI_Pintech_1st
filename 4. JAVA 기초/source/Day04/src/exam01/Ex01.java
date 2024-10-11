e45deee911944b0a48b932354b042a028fedd8fe
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-11 10:02:22 +0900

    Day4

--- 4. JAVA 기초/source/Day04/src/exam01/Ex01.java
+++ 4. JAVA 기초/source/Day04/src/exam01/Ex01.java
@@ -1,5 +1,14 @@
 package exam01;
 
 public class Ex01 {
-
+    public static void main(String[] args) {
+        //  void : 반환 값이 없을 때 자료형
+        //  return 예약어를 사용하지 않아도 됨.(자바 한정)
+        int r = add(10, 20);
+        System.out.println(r);
+    }
+    static int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
 }
