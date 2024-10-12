3de5200ec14ccb61e8219e3b76bcfc5ecfc8daa2
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-12 20:26:53 +0900

    .

--- 4. JAVA 기초/source/Day03/src/exam03/Ex01.java
+++ 4. JAVA 기초/source/Day03/src/exam03/Ex01.java
@@ -2,10 +2,15 @@ package exam03;
 
 public class Ex01 {
     public static void main(String[] args) {
+
+        // 실체화가 되어 메모리가 할당됨
+        // 실체화가 되었으니 인스턴스
         Student s1 = new Student();
-        s1.id = 1000;
-        s1.name = "김학생";
-        s1.major = "자바";
-        s1.study();
+
+        // 클래스 내 변수에 접근함.
+        s1.id = 1000; // 인스턴스 변수
+        s1.name = "김학생"; // 인스턴스 변수
+        s1.major = "자바"; // 인스턴스 변수
+        s1.study(); // 인스턴스 메서드
     }
 }
