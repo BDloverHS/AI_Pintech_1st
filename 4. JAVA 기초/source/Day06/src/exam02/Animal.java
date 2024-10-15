720eb4b6e38d6cb210fcd8207b5b77fa93bc91a3
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-15 18:21:25 +0900

    Day06

--- 4. JAVA 기초/source/Day06/src/exam02/Animal.java
+++ 4. JAVA 기초/source/Day06/src/exam02/Animal.java
@@ -1,7 +1,9 @@
 package exam02;
 
-public class Animal {
-    protected void move() {
-        System.out.println("움직인다.");
-    }
+public abstract class Animal {
+//    오류가 남
+//    public final void move() {
+//        System.out.println("움직인다.");
+//    };
+    public abstract void move();
 }
---
+++ 4. JAVA 기초/source/Day06/src/exam04/Math2.java
@@ -0,0 +1,8 @@
+package exam04;
+
+// Math에는 final이 붙어있어 상속불가하다.
+/*
+public class Math2 extends Math {
+
+}
+ */
