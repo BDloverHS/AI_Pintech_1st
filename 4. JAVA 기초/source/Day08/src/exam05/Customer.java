8a1c6821a706ad7c1dfa821a83deaed88d4ceef4
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-17 17:27:33 +0900

    Day08

--- 4. JAVA 기초/source/Day08/src/exam05/Customer.java
+++ 4. JAVA 기초/source/Day08/src/exam05/Customer.java
@@ -1,5 +1,7 @@
 package exam05;
 
+import java.util.Objects;
+
 public class Customer extends java.lang.Object {
     private int id; // 고객 번호
     private String name; // 고객명
@@ -12,7 +14,27 @@ public class Customer extends java.lang.Object {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Customer customer) {
+            // id, name, email이 같으면 동등하다
+            if (id == customer.id && name.equals(customer.name) && email.equals(customer.email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
     public String toString() {
-        return "id=" + id + ", name=" + name + ", email=" + email;
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
--- 4. JAVA 기초/source/Day08/src/exam05/Ex01.java
+++ 4. JAVA 기초/source/Day08/src/exam05/Ex01.java
@@ -3,6 +3,13 @@ package exam05;
 public class Ex01 {
     public static void main(String[] args) {
         Customer c1 = new Customer(1000,"이이름","user10@test.org");
+        Customer c2 = new Customer(1000,"이이름","user10@test.org");
         System.out.println(c1); // c1.toString()
+        System.out.printf("c1 == c2 : 동일성 ? %s%n", c1 == c2);
+        System.out.printf("c1.equals(c2) : 동등성 ? %s%n", c1.equals(c2));
+
+        System.out.printf("c1 주소 ? %s%n", System.identityHashCode(c1));
+        System.out.printf("c2 주소 ? %s%n", System.identityHashCode(c2));
+
     }
 }
