d2d7138aa19f623b91c239e8049ebee9f1d0cbdb
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-28 17:16:04 +0900

    .

--- 4. JAVA 기초/source/Day13/src/exam01/Book.java
+++ 4. JAVA 기초/source/Day13/src/exam01/Book.java
@@ -1,4 +1,13 @@
 package exam01;
 
 public class Book {
+    private String title;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
