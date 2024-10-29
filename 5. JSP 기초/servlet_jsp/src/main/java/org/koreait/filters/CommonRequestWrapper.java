3fedabe144a0695a3ff9a9c260cc6a1eb6c1b3fc
BDloverHS <gustn020702naver.com@gmail.com>
2024-10-29 13:04:05 +0900

    .

--- 5. JSP 기초/servlet_jsp/src/main/java/org/koreait/filters/CommonRequestWrapper.java
+++ 5. JSP 기초/servlet_jsp/src/main/java/org/koreait/filters/CommonRequestWrapper.java
@@ -11,4 +11,14 @@ public class CommonRequestWrapper extends HttpServletRequestWrapper {
         // 요청 시 공통 처리 코드....
         System.out.println("요청 공통 코드 처리...");
     }
+
+    @Override
+    public String getParameter(String name) {
+
+        String value = super.getParameter(name);
+        if (value != null || value.isBlank()) {
+            value = String.format("**%s**", value);
+        }
+        return value;
+    }
 }
