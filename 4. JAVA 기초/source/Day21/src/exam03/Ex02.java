9777053825e4e220db62c0b91deb913c989069ab
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-09 17:19:27 +0900

    Day21

--- 4. JAVA 기초/source/Day21/src/exam03/Ex02.java
+++ 4. JAVA 기초/source/Day21/src/exam03/Ex02.java
@@ -1,11 +1,25 @@
 package exam03;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
+import java.util.Locale;
 
 public class Ex02 {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
+        LocalDate today2 = LocalDate.of(4, 2, 29);
+
+        int year1 = today2.get(ChronoField.YEAR);
+        int month1 = today2.get(ChronoField.MONTH_OF_YEAR);
+        int day1 = today2.get(ChronoField.DAY_OF_MONTH);
+        DayOfWeek dayweek = today2.getDayOfWeek();
+
+        String yoil = dayweek.getDisplayName(TextStyle.SHORT, Locale.KOREA);
+        System.out.println(yoil + "요일");
+
+        System.out.println(today2);
 
         /*
         int year = today.get(ChronoField.YEAR);
@@ -16,8 +30,6 @@ public class Ex02 {
         int year = today.getYear();
         int month = today.getMonthValue();
         int day = today.getDayOfMonth();
-        System.out.printf("year=%d, month=%d, day%d%n", year, month, day);
-
         System.out.printf("year=%d, month=%d, day=%d%n", year, month, day);
     }
 }
